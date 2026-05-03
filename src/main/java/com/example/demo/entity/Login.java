c96a6f13f4b30ce335001dc3a7c79a7d41e26ad1
jorge mario mora <jorgemariomoramolina@gmail.com>
2026-05-02 22:11:00 -0500

    creando la entidad login

--- src/main/java/com/example/demo/entity/Login.java
+++ src/main/java/com/example/demo/entity/Login.java
@@ -1,5 +1,67 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+	
+@Entity
+@Table(name="login_usuarios")
 public class Login {
+		
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		@Column(name="log_id")
+		private long id;
+		@Column(name="log_username")
+		private String username;
+		@Column(name="log_pass")
+		private String Password;
+		
+		
+		public Login(long id, String username, String password) {
+			super();
+			this.id = id;
+			this.username = username;
+			Password = password;
+		}
+
+
+		public Login() {
+			super();
+		}
+
+
+		public long getId() {
+			return id;
+		}
+
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+
+		public String getUsername() {
+			return username;
+		}
+
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+
+		public String getPassword() {
+			return Password;
+		}
+
+
+		public void setPassword(String password) {
+			Password = password;
+		}
+		
 
 }
